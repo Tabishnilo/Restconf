bf5fc626ebf7820174caf868bfedccfc44381361
MohamedISmail <r.mohamedismail315@gmail.com>
2023-04-03 13:26:34 +0530

    Rest

---
+++ Restconf/Get_Retrieve.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from pprint import pprint
+
+device = {
+    "ip" : "10.63.255.61",
+    "username" : "admin",
+    "password" : "admin",
+    "port" : "443",
+
+}
+
+headers = {
+    "Accept" : "application/yang-data+json",
+    "content-type" : "application/yang-data+json",
+}
+
+module = "ietf-interfaces:interfaces"
+
+url = f"https://{device['ip']}:{device['port']}/restconf/data/{module}"
+
+
+requests.packages.urllib3.disable_warnings()
+response = requests.get(url,headers=headers,auth=(device['username'],device['password']), verify=False).json()
+
+
+interfaces = response['ietf-interfaces:interfaces']['interface']
+print (interfaces)
+
+
+for interface in interfaces:
+    if bool(interface[ 'ietf-ip:ipv4']): 
+        print(interface) 
